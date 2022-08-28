5873eb4d9c8dd92bdf72dc3b38dc99266ebbd91a
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-08-28 23:22:55 +0530

    conditionl render 1

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,16 @@
   <h1>{{ name }}</h1>
   <button @click="append" v-bind:disabled="isDisabled">Append</button>
   <div v-html="title"></div>
-  <div v-bind:id="ID" v-bind:class="[`container`,`danger`]">Hello</div>
-  <div :id="ID" class="underline" :class="status ? danger : safe">Conditional</div>
+  <div v-bind:id="ID" v-bind:class="[`container`, `danger`]">Hello</div>
+  <div :id="ID" class="underline" :class="status ? danger : safe">
+    Conditional
+  </div>
   <div :class="[!status && 'danger']">Color it 2</div>
-  <div :style="{color}">InLine styling Color it</div>
-
-    <div :style="stylishObject">InLine styling Color it</div>
+  <div :style="{ color }">InLine styling Color it</div>
+  <h2 v-if="num > 0">This is +ve</h2>
+  <h2 v-else-if="num < 0">This is -ve</h2>
+  <h2 v-else> This is Zero</h2>
+  <div :style="stylishObject">InLine styling Color it</div>
 </template>
 
 <script>
@@ -21,16 +25,17 @@ export default {
       ID: "specialOne",
       container: "wrapper",
       isDisabled: true,
-      status : false,
-      danger : "danger",
-      safe : "safe",
-      color : "violet",
-stylishObject:{
-backgroundColor: "yellow",
-color : "orange"
-,border : "1px solid violet",
-padding : "20px",
-},
+      status: false,
+      danger: "danger",
+      safe: "safe",
+      color: "violet",
+      stylishObject: {
+        backgroundColor: "yellow",
+        color: "orange",
+        border: "1px solid violet",
+        padding: "20px",
+      },
+      num: -1,
     };
   },
   methods: {
