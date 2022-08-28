6b31bad8c9f4327391a2c20fd6f6c23ba57ccd8b
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-08-28 12:03:52 +0530

    class attr binding

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>{{name}}</h1>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ name }}</h1>
   <button @click="append" v-bind:disabled="isDisabled">Append</button>
   <div v-html="title"></div>
-<div v-bind:id="ID" v-bind:class="container">Hello</div>
-<div :id="ID" :class="container">Hello</div>
+  <div v-bind:id="ID" v-bind:class="[`container`,`danger`]">Hello</div>
+  <div :id="ID" class="underline" :class="status ? danger : safe">Conditional</div>
+  <div :class="[!status && `danger`]">Color it 2</div>
+  <!-- underline -->
 </template>
 
 <script>
 export default {
-  name: 'App',
-data(){
-  return {
-    name : "Rushikesh",
-    title :"<h1 style=color:red>Hello</h1>",
-    ID : "specialOne",
-    container : "wrapper",
-    isDisabled : true,
-  }
-}
-,
-methods : {
-append(){
-  this.name+=" Tarapure" 
-}
-}
-}
+  name: "App",
+  data() {
+    return {
+      name: "Rushikesh",
+      title: "<h1 style=color:red>Hello</h1>",
+      ID: "specialOne",
+      container: "wrapper",
+      isDisabled: true,
+      status : false,
+      danger : "danger",
+      safe : "safe",
+      color : "yellow",
+    };
+  },
+  methods: {
+    append() {
+      this.name += " Tarapure";
+    },
+  },
+};
 </script>
 
 <style>
@@ -38,8 +43,20 @@ append(){
   margin-top: 60px;
 }
 
-.wrapper{
-  border : 1px solid black;
-  width : 50%;
+.underline {
+  text-decoration: underline;
+}
+
+.wrapper {
+  border: 1px solid black;
+  width: 50%;
+}
+
+.danger {
+  background-color: tomato;
+}
+
+.safe {
+  background-color: green;
 }
 </style>
